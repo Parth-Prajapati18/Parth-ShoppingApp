8c31db3859b5b2302e2cd8a89fe65d4372c95d18
Parth-Prajapati18 <parthb1806@gmail.com>
2023-08-03 08:21:17 -0400

    Editing dropdown

--- ecommerceapp/app/components/DropdownCheckbox.js
+++ ecommerceapp/app/components/DropdownCheckbox.js
@@ -1,54 +1,89 @@
-"use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import {AiOutlineDown, AiOutlineUp, AiFillCloseCircle} from 'react-icons/ai'
 
 export default function DropdownCheckbox({ DropTitle, items, selectedItems, onChange }) {
-
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleCheckboxChange = (item) => {
-    const isChecked = selectedItems.includes(item);
-    const updatedItems = isChecked
+  const handleClearSelection = () => {
+    onChange([]);
+  };
+
+  const handleItemClick = (item) => {
+    const updatedItems = selectedItems.includes(item)
       ? selectedItems.filter((selectedItem) => selectedItem !== item)
       : [...selectedItems, item];
 
     onChange(updatedItems);
   };
 
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleScroll = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='relative'>
-      <button
-        type="button"
-        className='w-full py-2 pl-3 pr-10 text-left bg-white border shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+    <div ref={dropdownRef} className='relative'>
+      <div
+        type='button'
+        className={`w-auto flex justify-between mx-2 py-2 pl-3 pr-10 text-left ${selectedItems.length > 0 ? 'bg-blue-200 border-blue-300 text-blue-500 font-normal' : 'bg-white border'
+          } border shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500`}
         onClick={handleToggle}
       >
-        {selectedItems.length === 0
-          ? DropTitle
-          : `${selectedItems[0]}${selectedItems.length > 1 ? ` (+${selectedItems.length - 1})` : ''}`}
-      </button>
+        <div>
+          {selectedItems.length === 0 ? DropTitle : `${selectedItems[0]}${selectedItems.length > 1 ? `+${selectedItems.length - 1}` : ''}`}
+        </div>
 
-      { isOpen && (
-        <div className='absolute w-full mt-2 bg-white border shadow-md'>
+        <div>
+          {selectedItems.length > 0 && (
+           <AiFillCloseCircle onClick={handleClearSelection}/>
+          )}
+
+          {isOpen ? (
+            <AiOutlineUp className={`${selectedItems.length > 0 ? 'hidden' : ''}`} />
+           
+          ) : (
+            <AiOutlineDown className={`${selectedItems.length > 0 ? 'hidden' : ''}`} /> 
+          )}
+        </div>
+
+
+      </div>
+
+      {isOpen && (
+        <div className='absolute w-full mt-2 bg-white border border-blue-300 shadow-md'>
           {items.map((item) => (
-            <label
-              key={item}
-              className="block px-4 py-2 text-sm cursor-pointer select-none"
-            >
+            <label key={item} className='block p-2 cursor-pointer'>
               <input
-                type="checkbox"
-                className="mr-2 leading-tight"
+                type='checkbox'
+                className='mr-2 leading-tight'
                 checked={selectedItems.includes(item)}
-                onChange={() => handleCheckboxChange(item)}
+                onChange={() => handleItemClick(item)}
               />
-              {item}
+              <span className='text-sm'>{item}</span>
             </label>
           ))}
         </div>
       )}
-
     </div>
-  )
+  );
 }
